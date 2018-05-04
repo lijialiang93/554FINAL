aa2ebf688ee9235746dc0e2c5af567f3c675e182
zz-chen-s <ckahome2007@gmail.com>
2018-05-04 01:13:10 -0400

    update seed

--- updates/0.0.1-admin.js
+++ updates/0.0.1-admin.js
@@ -10,6 +10,18 @@ exports = module.exports = function (done) {
     }).save();
 
     new Movie.model({
-        name: 'ROP'
+        name: 'RPO',
+        state: 'published',
+        director: '<p>11</p>',
+        genre: 'Action',
+        image: {
+            filename: '671513e1-415c-4bf9-8387-b716cf1f855a.jpg',
+            size: 227195,
+            mimetype: 'image/jpeg'
+        },
+        mpaa: 'PG-13',
+        runningTime: 140,
+        stars: '<p>22</p>',
+        storyline: '<p>3333</p>'
     }).save(done);
 };
