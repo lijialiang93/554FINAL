e507ed4e718c65b36b6b236570b5caf739284f22
lijialiang93 <lijialiang93@gmail.com>
2018-05-05 01:14:06 -0400

    add worker

---
+++ worker/data/movie.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/554tf');
+let movieSchema = new mongoose.Schema({}, { strict: false });
+let Movie = mongoose.model('Movie', movieSchema);
+
+const exportedMethods = {
+
+    async getMovieByName(name) {
+
+        try {
+            return await Movie.findOne({ 'name': name });
+        } catch (error) {
+            console.log(error);
+        }
+
+    },
+
+    async getMovieById(id) {
+        try {
+            return await Movie.findOne({'_id': id });
+        } catch (error) {
+            console.log(error);
+        }
+
+    }
+}
+
+module.exports = exportedMethods;
