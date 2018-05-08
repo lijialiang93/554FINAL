86cb93aae052994f83ff17990768e486bfa0943a
zp1993 <zepusong@gmail.com>
2018-05-08 18:25:24 -0400

    fix reviewentry

--- client/components/review/ReviewEntry.jsx
+++ client/components/review/ReviewEntry.jsx
@@ -4,9 +4,9 @@ function ReviewEntry({ review }) {
 return (
     <div className="card">
       <div className="card-body">
-      <h5 className="card-title">{review.author}}</h5>
-      <h6 className="card-subtitle mb-2 text-muted">{review.createdAt}}</h6>
-      <p className="card-text">{review.content}}</p>
+      <h5 className="card-title">{review.author}</h5>
+      <h6 className="card-subtitle mb-2 text-muted">{review.createdAt}</h6>
+      <p className="card-text">{review.content}</p>
   </div>
 </div>
   )
