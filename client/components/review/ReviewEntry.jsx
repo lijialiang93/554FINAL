cddd327fcce7da8ed4924a6115672b5dc9acd8d4
lijialiang93 <lijialiang93@gmail.com>
2018-05-10 22:20:03 -0400

    fix avatar size

--- client/components/review/ReviewEntry.jsx
+++ client/components/review/ReviewEntry.jsx
@@ -1,21 +1,22 @@
 import React from "react";
 
 function ReviewEntry({ review }) {
-return (
+  return (
     <div className="card">
+
       <div className="card-body">
-      <div className="row">
-      <div className="col-3">
-      <img src={review.avatar}/>
+        <div className="row">
+          <div className="col-3">
+            <img class="card-img-top" src={review.avatar} alt="Card image cap" />
+          </div>
+          <div className="col-9">
+            <h5 className="card-title">{review.author}</h5>
+            <h6 className="card-subtitle mb-2 text-muted">{new Date(review.createdAt).toLocaleString()}</h6>
+          </div>
+        </div>
+        <p className="card-text">{review.content}</p>
       </div>
-      <div className="col-9">
-      <h5 className="card-title">{review.author}</h5>
-      <h6 className="card-subtitle mb-2 text-muted">{new Date(review.createdAt).toLocaleString()}</h6>
-      </div>
-      </div>
-      <p className="card-text">{review.content}</p>
-  </div>
-</div>
+    </div>
   )
 }
 
